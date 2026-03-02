aaa3c679ef7d051a02f7aabdbe25b4d777dfb308
TheFartingMan <98778454+TheFartingMan@users.noreply.github.com>
2026-03-02 13:12:19 -0800

    Ground Check

--- Assets/Scripts/StateMachineComponents/PlayerGroundCheck.cs
+++ Assets/Scripts/StateMachineComponents/PlayerGroundCheck.cs
@@ -6,11 +6,28 @@ public class PlayerGroundCheck : MonoBehaviour
 
     void OnCollisionStay(Collision collision)
     {
-        isGrounded = true;
+        ContactPoint contact = collision.contacts[0];
+        int groundContacts = collision.contactCount;
+
+            foreach (ContactPoint c in collision.contacts)
+            {
+                if (collisionAngle(c) < 30)
+                {
+                    isGrounded = true;
+                }
+            }
     }
 
     void OnCollisionExit(Collision collision)
     {
         isGrounded = false;
+    }
+
+
+    private float collisionAngle(ContactPoint contact)
+    {
+        Vector3 surfaceNormal = contact.normal;
+        float slopeAngle = Vector3.Angle(surfaceNormal, Vector3.up);
+        return slopeAngle;
     }
 }
