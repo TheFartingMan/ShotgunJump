91be0e424591546c6fbb8532cf9e7b62e914b1ba
TheFartingMan <98778454+TheFartingMan@users.noreply.github.com>
2026-05-10 20:58:19 -0700

    Added Enemy/other stuff

---
+++ Assets/Scripts/EnemyStuff/VisorTextScroller.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class VisorTextScroller : MonoBehaviour
+{
+    [SerializeField] private GameObject textObject;
+    [SerializeField] private float textScrollSpeed;
+    private RectTransform textObjectRectTransform;
+
+    void Start()
+    {
+        textObjectRectTransform = textObject.GetComponent<RectTransform>();
+    }
+    
+    void Update()
+    {
+        textObjectRectTransform.localPosition -= Vector3.forward * textScrollSpeed * Time.deltaTime;
+
+        if (textObjectRectTransform.localPosition.z < -(textObjectRectTransform.rect.width * .1f + .5f))
+        {
+            textObjectRectTransform.localPosition = new Vector3(textObjectRectTransform.localPosition.x, textObjectRectTransform.localPosition.y, 0.5f);
+        }
+    }
+}
